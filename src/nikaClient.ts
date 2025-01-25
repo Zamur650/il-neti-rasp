26757aaf15016609360e34a0e1ed54a52d70bc2f
Aleksei Rybin <aleksei@tapni.su>
2025-01-25 13:33:30 +0700

    fix nikaurl

--- src/nikaClient.ts
+++ src/nikaClient.ts
@@ -1,11 +1,11 @@
-import { NikaResponse } from "nikaResponse.js";
+import { NikaResponse } from "./nikaResponse.js";
 
 export class NikaClient {
   async getData() {
     const websiteHtml = await fetch(
       "https://lyceum.nstu.ru/rasp/schedule.html"
     ).then((res) => res.text());
-    const nikaUrl = websiteHtml.substring(450, 478);
+    const nikaUrl = `https://lyceum.nstu.ru/rasp/${websiteHtml.substring(450, 478)}`;
     const nikaScript = await fetch(nikaUrl).then((res) => res.text());
 
     var NIKA: NikaResponse;
